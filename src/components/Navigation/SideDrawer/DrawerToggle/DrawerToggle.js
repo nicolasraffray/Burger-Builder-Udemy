f729c14cf4943a035f7c958e9aaa54393c97b4a2
nicolasraffray <nraffray@gmail.com>
2020-05-15 20:15:48 +0100

    icon instead of menu word

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,12 @@
 import React from "react";
+import classes from "./DrawerToggle.module.css";
 
-const drawToggle = (props) => <div onClick={props.clicked}>MENU</div>;
+const drawToggle = (props) => (
+  <div onClick={props.clicked} className={classes.DrawerToggle}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default drawToggle;
