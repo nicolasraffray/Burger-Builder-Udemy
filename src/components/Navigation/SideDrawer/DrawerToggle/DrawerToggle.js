efbca51768a37aa2cb15cac3050080dcddaf7fbb
nicolasraffray <nraffray@gmail.com>
2020-05-15 20:06:57 +0100

    can click menu to toggle side drawer

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const drawToggle = (props) => <div onClick={props.clicked}>MENU</div>;
+
+export default drawToggle;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from "react";
 import styles from "./Toolbar.module.css";
 import Logo from "../../Layout/Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = (props) => (
   <header className={styles.Toolbar}>
-    <div>MENU</div>
+    <DrawToggle clicked={props.drawerToggleClicked} />
     <div className={styles.Logo}>
       <Logo />
     </div>
