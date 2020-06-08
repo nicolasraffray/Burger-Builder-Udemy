764e41fdff00fae63fdcb57f07156903cecda4bf
nicolasraffray <nraffray@gmail.com>
2020-06-08 12:10:14 +0100

    routing added to checkout summary, routing to /checkout/contact-data

--- src/components/containers/Checkout/Checkout.js
+++ src/components/containers/Checkout/Checkout.js
@@ -10,10 +10,23 @@ class Checkout extends Component {
       beacon: 1,
     },
   };
+
+  checkoutCanceledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCanceled={this.checkoutCanceledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
     );
   }
