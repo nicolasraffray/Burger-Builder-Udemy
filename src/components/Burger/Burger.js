cf3a23521d434b0edf73858acee75b495feb658f
nicolasraffray <nraffray@gmail.com>
2020-06-08 11:50:22 +0100

    some routing implemented, make order click continue to see

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import styles from './Burger.module.css'
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react";
+import styles from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import { withRouter } from "react-router-dom";
 
 const burger = (props) => {
-  let ingredients = Object.keys(props.ingredient).map(ingredientKey => {
-    return [...Array(props.ingredient[ingredientKey])].map((_, i) => {
-      return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+  let ingredients = Object.keys(props.ingredient)
+    .map((ingredientKey) => {
+      return [...Array(props.ingredient[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
     })
-  }).reduce((prevEl, nextEl) => {
-    return prevEl.concat(nextEl)
-  }, [])
+    .reduce((prevEl, nextEl) => {
+      return prevEl.concat(nextEl);
+    }, []);
 
-  if(ingredients.length === 0){
-    ingredients = <p>Please Add Food</p>
+  if (ingredients.length === 0) {
+    ingredients = <p>Please Add Food</p>;
   }
-  
-  return(
-    <div className={styles.Burger}>
-      <BurgerIngredient type={"bread-top"}/>  
-      {ingredients}
-      <BurgerIngredient type={"bread-bottom"}/>  
-    </div>
-  )
-}
 
-export default burger; 
+  return (
+    <div className={styles.Burger}>
+      <BurgerIngredient type={"bread-top"} />
+      {ingredients}
+      <BurgerIngredient type={"bread-bottom"} />
+    </div>
+  );
+};
+
+export default burger;
--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -103,26 +103,27 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({
-      loading: true,
-    });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Test Name",
-        adress: {
-          street: "testStreet",
-          zip: "testZip",
-        },
-        email: "test@example.com",
-      },
-      deliveryMethod: "fast",
-    };
-    axios
-      .post("/orders.json", order)
-      .then((res) => this.setState({ loading: false, purchasing: false }))
-      .catch((err) => this.setState({ loading: false, purchasing: false }));
+    // this.setState({
+    //   loading: true,
+    // });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Test Name",
+    //     adress: {
+    //       street: "testStreet",
+    //       zip: "testZip",
+    //     },
+    //     email: "test@example.com",
+    //   },
+    //   deliveryMethod: "fast",
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then((res) => this.setState({ loading: false, purchasing: false }))
+    //   .catch((err) => this.setState({ loading: false, purchasing: false }));
+    this.props.history.push("/checkout");
   };
 
   render() {
